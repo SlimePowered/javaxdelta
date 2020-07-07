30254c1dea10d89a410ed3b29951b31906ee7c19
hYdos <haydenv06@gmail.com>
2020-07-07 11:23:33 +1000

    initial commit (saved from curseforge)

---
+++ src/main/java/at/spardat/xma/xdelta/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package for creating delta files for .jar files.
+ * 
+ * Refer to {@link at.spardat.xma.xdelta.JarDelta} and {@link at.spardat.xma.xdelta.JarPatcher} for details.
+ */
+package at.spardat.xma.xdelta;
