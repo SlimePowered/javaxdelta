02cfa2f83e38e3317224b649885bcdc24b03b772
hYdos <haydenv06@gmail.com>
2021-05-23 07:44:59 +1000

    fix spec

--- src/main/java/com/nothome/delta/GDiffPatcher.java
+++ src/main/java/com/nothome/delta/GDiffPatcher.java
@@ -25,40 +25,19 @@
 
 package com.nothome.delta;
 
-import static com.nothome.delta.GDiffWriter.COPY_INT_INT;
-import static com.nothome.delta.GDiffWriter.COPY_INT_UBYTE;
-import static com.nothome.delta.GDiffWriter.COPY_INT_USHORT;
-import static com.nothome.delta.GDiffWriter.COPY_LONG_INT;
-import static com.nothome.delta.GDiffWriter.COPY_USHORT_INT;
-import static com.nothome.delta.GDiffWriter.COPY_USHORT_UBYTE;
-import static com.nothome.delta.GDiffWriter.COPY_USHORT_USHORT;
-import static com.nothome.delta.GDiffWriter.DATA_INT;
-import static com.nothome.delta.GDiffWriter.DATA_MAX;
-import static com.nothome.delta.GDiffWriter.DATA_USHORT;
-import static com.nothome.delta.GDiffWriter.EOF;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.nio.ByteBuffer;
+
+import static com.nothome.delta.GDiffWriter.*;
 
 /**
  * This class patches an input file with a GDIFF patch file.
- *
+ * <p>
  * The patch file follows the GDIFF file specification available at
  * {@link http://www.w3.org/TR/NOTE-gdiff-19970901.html}.
  */
 public class GDiffPatcher {
-    
+
     private ByteBuffer buf = ByteBuffer.allocate(1024);
     private byte buf2[] = buf.array();
 
@@ -67,14 +46,13 @@ public class GDiffPatcher {
      */
     public GDiffPatcher() {
     }
-    
+
     /**
      * Patches to an output file.
      */
     public void patch(File sourceFile, File patchFile, File outputFile)
-		throws IOException
-	{
-        RandomAccessFileSeekableSource source =new RandomAccessFileSeekableSource(new RandomAccessFile(sourceFile, "r")); 
+            throws IOException {
+        RandomAccessFileSeekableSource source = new RandomAccessFileSeekableSource(new RandomAccessFile(sourceFile, "r"));
         InputStream patch = new FileInputStream(patchFile);
         OutputStream output = new FileOutputStream(outputFile);
         try {
@@ -87,14 +65,14 @@ public class GDiffPatcher {
             output.close();
         }
     }
-    
+
     /**
      * Patches to an output stream.
      */
     public void patch(byte[] source, InputStream patch, OutputStream output) throws IOException {
         patch(new ByteBufferSeekableSource(source), patch, output);
     }
-    
+
     /**
      * Patches in memory, returning the patch result.
      */
@@ -103,12 +81,12 @@ public class GDiffPatcher {
         patch(source, new ByteArrayInputStream(patch), os);
         return os.toByteArray();
     }
-    
+
     /**
      * Patches to an output stream.
      */
     public void patch(SeekableSource source, InputStream patch, OutputStream out) throws IOException {
-        
+
         DataOutputStream outOS = new DataOutputStream(out);
         DataInputStream patchIS = new DataInputStream(patch);
 
@@ -123,71 +101,75 @@ public class GDiffPatcher {
         }
 
         while (true) {
-            int command = patchIS.readUnsignedByte();
+            int command;
+            try {
+                command = patchIS.readUnsignedByte();
+            } catch (EOFException ignored) {
+                command = 0;
+            }
             if (command == EOF)
                 break;
             int length;
             int offset;
-            
+
             if (command <= DATA_MAX) {
                 append(command, patchIS, outOS);
                 continue;
             }
-            
+
             switch (command) {
-            case DATA_USHORT: // ushort, n bytes following; append
-                length = patchIS.readUnsignedShort();
-                append(length, patchIS, outOS);
-                break;
-            case DATA_INT: // int, n bytes following; append
-                length = patchIS.readInt();
-                append(length, patchIS, outOS);
-                break;
-            case COPY_USHORT_UBYTE:
-                offset = patchIS.readUnsignedShort();
-                length = patchIS.readUnsignedByte();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_USHORT_USHORT:
-                offset = patchIS.readUnsignedShort();
-                length = patchIS.readUnsignedShort();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_USHORT_INT:
-                offset = patchIS.readUnsignedShort();
-                length = patchIS.readInt();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_INT_UBYTE:
-                offset = patchIS.readInt();
-                length = patchIS.readUnsignedByte();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_INT_USHORT:
-                offset = patchIS.readInt();
-                length = patchIS.readUnsignedShort();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_INT_INT:
-                offset = patchIS.readInt();
-                length = patchIS.readInt();
-                copy(offset, length, source, outOS);
-                break;
-            case COPY_LONG_INT:
-                long loffset = patchIS.readLong();
-                length = patchIS.readInt();
-                copy(loffset, length, source, outOS);
-                break;
-            default: 
-                throw new IllegalStateException("command " + command);
+                case DATA_USHORT: // ushort, n bytes following; append
+                    length = patchIS.readUnsignedShort();
+                    append(length, patchIS, outOS);
+                    break;
+                case DATA_INT: // int, n bytes following; append
+                    length = patchIS.readInt();
+                    append(length, patchIS, outOS);
+                    break;
+                case COPY_USHORT_UBYTE:
+                    offset = patchIS.readUnsignedShort();
+                    length = patchIS.readUnsignedByte();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_USHORT_USHORT:
+                    offset = patchIS.readUnsignedShort();
+                    length = patchIS.readUnsignedShort();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_USHORT_INT:
+                    offset = patchIS.readUnsignedShort();
+                    length = patchIS.readInt();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_INT_UBYTE:
+                    offset = patchIS.readInt();
+                    length = patchIS.readUnsignedByte();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_INT_USHORT:
+                    offset = patchIS.readInt();
+                    length = patchIS.readUnsignedShort();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_INT_INT:
+                    offset = patchIS.readInt();
+                    length = patchIS.readInt();
+                    copy(offset, length, source, outOS);
+                    break;
+                case COPY_LONG_INT:
+                    long loffset = patchIS.readLong();
+                    length = patchIS.readInt();
+                    copy(loffset, length, source, outOS);
+                    break;
+                default:
+                    throw new IllegalStateException("command " + command);
             }
         }
-		outOS.flush();
+        outOS.flush();
     }
 
     private void copy(long offset, int length, SeekableSource source, OutputStream output)
-		throws IOException
-	{
+            throws IOException {
         source.seek(offset);
         while (length > 0) {
             int len = Math.min(buf.capacity(), length);
@@ -203,9 +185,9 @@ public class GDiffPatcher {
     private void append(int length, InputStream patch, OutputStream output) throws IOException {
         while (length > 0) {
             int len = Math.min(buf2.length, length);
-    	    int res = patch.read(buf2, 0, len);
-    	    if (res == -1)
-    	        throw new EOFException("cannot read " + length);
+            int res = patch.read(buf2, 0, len);
+            if (res == -1)
+                throw new EOFException("cannot read " + length);
             output.write(buf2, 0, res);
             length -= res;
         }
@@ -227,7 +209,7 @@ public class GDiffPatcher {
             File outputFile = new File(argv[2]);
 
             if (sourceFile.length() > Integer.MAX_VALUE ||
-            patchFile.length() > Integer.MAX_VALUE) {
+                    patchFile.length() > Integer.MAX_VALUE) {
                 System.err.println("source or patch is too large, max length is " + Integer.MAX_VALUE);
                 System.err.println("aborting..");
                 return;
